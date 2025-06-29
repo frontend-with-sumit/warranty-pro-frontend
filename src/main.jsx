889ef6f16e69305c4b0158e8f99b74ca45571794
Sumit <sk990005@outlook.com>
2025-06-29 15:51:14 +0530

    added routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,3 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { Provider } from './components/ui/provider';
 import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/500.css';
 import '@fontsource/poppins/600.css';
@@ -15,11 +10,17 @@ import '@fontsource/work-sans/600.css';
 import '@fontsource/work-sans/700.css';
 import '@fontsource/work-sans/800.css';
 import '@fontsource/work-sans/900.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router';
+import { Provider } from './components/ui/provider';
+import './index.css';
+import router from './routes/router';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<Provider>
-			<App />
+			<RouterProvider router={router} />
 		</Provider>
 	</StrictMode>
 );
